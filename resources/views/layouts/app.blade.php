9b2706707d5bd76363e27c73205c753b7834951f
Tongai <tongai@avochoc.com>
2018-12-06 16:53:24 +0200

    Minor cleanup dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <!-- Styles -->
     {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
 </head>
-<body >
+<body style="background: url('/images/background_image.png');background-size:cover">
 <div id="app">
     {{--<nav class="navbar navbar-expand-md navbar-light navbar-laravel">--}}
     {{--<div class="container">--}}
--- resources/views/layouts/user_logged.blade.php
+++ resources/views/layouts/user_logged.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Munitask') }}</title>
+    <title>{{ config('app.name', 'SMAT') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -19,27 +19,25 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-   <link href="/css/side-nav.css" rel="stylesheet"/>
+
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="/css/w3_nav_side.css" rel="stylesheet"/>
 </head>
 <body>
 <div id="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <button type="button" id="sidebarCollapse" class="btn btn-info">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
+        <button id="openNav" class="w3-button w3-teal w3-xlarge" onclick="w3_open()">&#9776;</button>
         <a class="navbar-brand mx-auto" href="/home" style="margin-left: 2em;">SMAT</a>
-
     </nav>
 
-    <div id="mySidenav" class="sidenav nav-side-menu">
-        <a href="javascript:void(0)" class="closebtn" style="padding:0px;" onclick="closeNav()">&times;</a>
-        <div class="" style="margin-top:3em;">
-            <div class="card hovercard" style="width: 300px;">
-                <div class="cardheader">
-                </div>
+    <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
+        <button class="w3-bar-item w3-button w3-large"
+                onclick="w3_close()">Close &times;</button>
+        <div>
+            <div class="card hovercard" >
+
                 <div class="avatar">
-                    <img alt="" src="http://lorempixel.com/100/100/people/9/">
+                    <img alt="" src="{{!is_null($user->profile_picture_url)?$user->profile_picture_url:'/images/profile_placeholder.jpg'}}"/>
                 </div>
                 <div class="info">
                     <div class="title">
@@ -67,11 +65,6 @@
                 <li>
                     <a href="#"><i class="material-icons">assessment</i> Reports</a>
                 </li>
-                <ul class="sub-menu collapse" id="new">
-                    <li>New New 1</li>
-                    <li>New New 2</li>
-                    <li>New New 3</li>
-                </ul>
 
                 <li>
                     <a href="#">
@@ -87,66 +80,37 @@
             </ul>
         </div>
     </div>
-
-    {{--<nav class="navbar navbar-expand-md navbar-light navbar-laravel">--}}
-    {{--<div class="container">--}}
-    {{--<a class="navbar-brand" href="{{ url('/') }}">--}}
-    {{--{{ config('app.name', 'Laravel') }}--}}
-    {{--</a>--}}
-    {{--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
-    {{--<span class="navbar-toggler-icon"></span>--}}
-    {{--</button>--}}
-
-    {{--<div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
-    {{--<!-- Left Side Of Navbar -->--}}
-    {{--<ul class="navbar-nav mr-auto">--}}
-
-    {{--</ul>--}}
-
-    {{--<!-- Right Side Of Navbar -->--}}
-    {{--<ul class="navbar-nav ml-auto">--}}
-    {{--<!-- Authentication Links -->--}}
-    {{--@guest--}}
-    {{--<li class="nav-item">--}}
-    {{--<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
-    {{--</li>--}}
-    {{--<li class="nav-item">--}}
-    {{--@if (Route::has('register'))--}}
-    {{--<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
-    {{--@endif--}}
-    {{--</li>--}}
-    {{--@else--}}
-    {{--<li class="nav-item dropdown">--}}
-    {{--<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
-    {{--{{ Auth::user()->name }} <span class="caret"></span>--}}
-    {{--</a>--}}
-
-    {{--<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
-    {{--<a class="dropdown-item" href="{{ route('logout') }}"--}}
-    {{--onclick="event.preventDefault();--}}
-    {{--document.getElementById('logout-form').submit();">--}}
-    {{--{{ __('Logout') }}--}}
-    {{--</a>--}}
-
-    {{--<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
-    {{--@csrf--}}
-    {{--</form>--}}
-    {{--</div>--}}
-    {{--</li>--}}
-    {{--@endguest--}}
-    {{--</ul>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--</nav>--}}
-
+    <div id="main">
+    <div class="w3-container">
+        @yield('content')
+    </div>
+    </div>
 
 </div>
 <script>
-    $(document).ready(function(){
-       $('#sidebarCollapse').on('click',function(){
-          $("#mySidenav").css('width','300px');
-       });
-    });
+    function w3_open() {
+        let width = $(window).width();
+        if(width>768){
+            document.getElementById("main").style.marginLeft = "20%";
+            document.getElementById("mySidebar").style.width = "20%";
+            document.getElementById("mySidebar").style.display = "block";
+            document.getElementById("openNav").style.display = 'none';
+        }else{
+            document.getElementById("main").style.marginLeft = "50%";
+            document.getElementById("mySidebar").style.width = "50%";
+            document.getElementById("mySidebar").style.display = "block";
+            document.getElementById("openNav").style.display = 'none';
+        }
+
+    }
+    function w3_close() {
+        document.getElementById("main").style.marginLeft = "0%";
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("openNav").style.display = "inline-block";
+    }
+    window.onresize = function(event) {
+        w3_open();
+    };
     function closeNav() {
         $("#mySidenav").css('width','0px');
     }
